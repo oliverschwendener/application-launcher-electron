e01f898d3ce8221b1e2da944fec06f4b7fbfe131
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-06-10 16:33:31 +0200

    Added CommandlineUtility

---
+++ src/main/Utilities/CommandlineUtility.ts
@@ -0,0 +1,17 @@
+import { exec } from "child_process";
+
+export class CommandlineUtility {
+    public static executeCommandWithOutput(command: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                } else if (stderr) {
+                    reject(stderr);
+                } else {
+                    resolve(stdout.toString());
+                }
+            });
+        });
+    }
+}

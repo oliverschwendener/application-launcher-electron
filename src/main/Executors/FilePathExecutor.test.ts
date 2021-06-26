74f22f8921cb53873207a529e89dc38d78f98f35
Oliver Schwendener <oliver.schwendener@hotmail.com>
2021-06-26 23:35:29 +0200

    Removed redundant async keyword in unit test

--- src/main/Executors/FilePathExecutor.test.ts
+++ src/main/Executors/FilePathExecutor.test.ts
@@ -2,14 +2,14 @@ import { FilePathExecutor } from "./FilePathExecutor";
 import { SearchResultItemDummy } from "../../common/SearchResult/SearchResultItemDummy";
 
 describe(FilePathExecutor, () => {
-    it("should succeed if the file path opener resolves", async (done) => {
+    it("should succeed if the file path opener resolves", (done) => {
         new FilePathExecutor(() => Promise.resolve())
             .execute(SearchResultItemDummy.empty())
             .then(() => done())
             .catch((error) => done(error));
     });
 
-    it("should fail if the file path opener rejects", async (done) => {
+    it("should fail if the file path opener rejects", (done) => {
         new FilePathExecutor(() => Promise.reject("Failed"))
             .execute(SearchResultItemDummy.empty())
             .then(() => done("Should have failed"))
